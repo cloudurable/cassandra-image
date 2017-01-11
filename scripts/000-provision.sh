168b20817a8f0ecb394340c480bab1bc9583890a
jeanpaulazara <jeanpaulazar1980@gmail.com>
2017-01-10 16:08:28 -0800

    Added OS tuning, jemalloc, JNA, and NTP for clock sync.

--- scripts/000-provision.sh
+++ scripts/000-provision.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-yum -y install wget
+yum install -y wget
 yum install -y nano
 yum install -y java-1.8.0-openjdk
+yum install -y ntp
+yum install -y jna
+
+yum install -y epel-release
+yum install -y jemalloc
---
+++ scripts/010-tune-os.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Remove constraint on processes
+rm -f /etc/security/limits.d/*-nproc.conf
+## Copy our limits.conf
+cp ~/resources/limits.conf /etc/security/limits.conf
+## Copy our sysctl.conf
+cp  ~/resources/sysctl.conf /etc/sysctl.conf
+## Disable huge pages which can cause a CPU spike.
+cp ~/resources/initd-disable-transparent-hugepages /etc/init.d/disable-transparent-hugepages
+chmod 755 /etc/init.d/disable-transparent-hugepages
