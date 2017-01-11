a99f9296f0d508e05bc8c34d62d313a03686cb51
jeanpaulazara <jeanpaulazar1980@gmail.com>
2017-01-11 14:41:08 -0800

    Changed ports

--- bin/start-image.sh
+++ bin/start-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 docker rm cassie
-docker run  -it -p 7000:17000 -p 7199:17199 -p 9042:19042 --name cassie  cloudurable/cassandra:0.4
+docker run  -it -p 17000:7000 -p 17199:7199 -p 19042:9042 --name cassie  cloudurable/cassandra:0.4
