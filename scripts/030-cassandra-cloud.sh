664f72eb9795e3895c287f22f038f5b123d11e38
jeanpaulazara <jeanpaulazar1980@gmail.com>
2017-01-31 00:02:11 -0800

    Tested cluster.

--- scripts/030-cassandra-cloud.sh
+++ scripts/030-cassandra-cloud.sh
@@ -5,10 +5,12 @@ set -e
 mkdir -p /tmp/cassandra-cloud-install
 cd /tmp/cassandra-cloud-install
 
-wget https://github.com/cloudurable/cassandra-cloud/releases/download/v0.0.3/cassandra-cloud
+wget https://github.com/cloudurable/cassandra-cloud/releases/download/v0.0.4/cassandra-cloud
 mv cassandra-cloud /opt/cassandra/bin/cassandra-cloud
 chmod +x /opt/cassandra/bin/cassandra-cloud
 
 rm  -rf /tmp/cassandra-cloud-install
+
+cd ~
 
 echo "Done"
