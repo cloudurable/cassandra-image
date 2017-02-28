966b5fc04cebd05d0bb4f10d5ed09cb59f6dd261
jeanpaulazara <jeanpaulazar1980@gmail.com>
2017-02-28 15:24:34 -0800

    Putting in missing config files.

--- scripts/050-systemd-setup.sh
+++ scripts/050-systemd-setup.sh
@@ -8,10 +8,12 @@ systemctl start  cassandra
 
 
 cp ~/resources/etc/systemd/system/metricsd.service /etc/systemd/system/metricsd.service
+cp ~/resources/etc/metricsd.conf /etc/metricsd.conf
 systemctl enable metricsd
 systemctl start  metricsd
 
 cp ~/resources/etc/systemd/system/systemd-cloud-watch.service /etc/systemd/system/systemd-cloud-watch.service
+cp ~/resources/etc/systemd-cloud-watch.conf /etc/systemd-cloud-watch.conf
 systemctl enable systemd-cloud-watch
 systemctl start  systemd-cloud-watch
 
