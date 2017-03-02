bf267b0c946c734d939b48a3ca02dc959306f9df
jeanpaulazara <jeanpaulazar1980@gmail.com>
2017-03-01 17:00:22 -0800

    progress with ansible

--- ansible-ec2/install.sh
+++ ansible-ec2/install.sh
@@ -3,13 +3,18 @@
 if [ ! -d "ansible-ec2/ec2.py" ]; then
     wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O ansible-ec2/ec2.py
     chmod +x ansible-ec2/ec2.py
+    sudo cp ansible-ec2/ec2.py /etc/ansible/ec2.py
     wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini -O ansible-ec2/ec2.ini
+    sudo cp ansible-ec2/ec2.ini /etc/ansible/ec2.ini
 fi
 
-export ANSIBLE_HOSTS="$PWD/ansible-ec2/ec2.py"
-export EC2_INI_PATH="$PWD/ansible-ec2/ec2.ini"
+export ANSIBLE_HOSTS=/etc/ansible/ec2.py
+export EC2_INI_PATH=/etc/ansible/ec2.ini
 
-source bin/ec2-env.sh
+
+echo export ANSIBLE_HOSTS="$PWD/ansible-ec2/ec2.py"
+echo export EC2_INI_PATH="$PWD/ansible-ec2/ec2.ini"
+echo source bin/ec2-env.sh
 
 #
 echo "Things to try"
