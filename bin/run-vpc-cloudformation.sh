d74d6a9e732b1d091e975cad2c5cd1c5573f9581
jeanpaulazara <jeanpaulazar1980@gmail.com>
2017-03-05 18:23:31 -0800

    Started using CloudFormation to form Cassandra Cloud

---
+++ bin/run-vpc-cloudformation.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+source bin/ec2-env.sh
+
+aws --region ${REGION} s3 cp cloud-formation/vpc.json s3://$CLOUD_FORMER_BUCKET
+aws --region ${REGION} cloudformation create-stack --stack-name ${ENV}-vpc-cassandra \
+--template-url "https://s3-us-west-2.amazonaws.com/$CLOUD_FORMER_BUCKET/vpc.json" \
+
+#--parameters ParameterKey=VPCStackName,ParameterValue=${ENV}-vpc \
+#ParameterKey=KeyName,ParameterValue=${KEY} \
+#ParameterKey=Environment,ParameterValue=${ENV}
